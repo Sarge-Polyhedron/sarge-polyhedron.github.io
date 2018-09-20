2933c679472298e043816678b1f3a8a8e432212d
Sarge-Polyhedron <42914811+Sarge-Polyhedron@users.noreply.github.com>
2018-09-20 16:28:23 +0300

    update

--- Project1/js/common.js
+++ Project1/js/common.js
@@ -1,11 +1,18 @@
 $(function() {
 
+	// Easy TABS
 	$(".tab_item").not(":first").hide();
 	$(".tab").click(function() {
 		$(".tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tab_item").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
+	}).eq(0).addClass("active"); // Easy TABS
 
+
+	// change Active on click
+	$(".menu_item").click(function() {
+		$(".menu_item").removeClass("active");
+		$(this).addClass("active");
+	}); // change Active on click
 
 });
 
